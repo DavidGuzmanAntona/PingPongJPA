54dfbc911b7463bb015f262650d869b42b422765
PEPINATOR2019\Jamonete <Hola>
2024-01-27 10:59:02 +0100

    AÑADIDOS DATOS ORIGINALES JUGADOR, EQUIPO, ESTADIO 	https://rfetm.es/resultados/2023-2024/view.php?liga=MQ==&grupo=0&subgrupo=S&jornada=0&sexo=F

---
+++ src/main/java/entidades/Estadio.java
@@ -0,0 +1,93 @@
+package entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "stadium")
+public class Estadio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IdStadium")
+	private Long idEstadio;
+
+	@Column(name = "name")
+	private String nombre;
+
+	@Column(name = "location")
+	private String ubicacion;
+
+	@Column(name = "capacity")
+	private int capacidad;
+
+//relaciones
+
+	@OneToOne
+	@JoinColumn(name = "equipo_id")
+	private Equipo equipo;
+
+//Constructores	
+	public Estadio() {
+		super();
+	}
+
+	public Estadio(String nombre, String ubicacion, int capacidad) {
+		super();
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+		this.capacidad = capacidad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+
+	public Long getIdEstadio() {
+		return idEstadio;
+	}
+
+	@Override
+	public String toString() {
+		return "Estadio [idEstadio=" + idEstadio + ", nombre=" + nombre + ", ubicacion=" + ubicacion + ", capacidad="
+				+ capacidad + "]";
+	}
+	
+	
+	
+
+}
